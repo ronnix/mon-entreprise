bd3c8b788d43a283ce201d83d11e028f9fb505c3
Maxime Quandalle <maxime@quandalle.com>
2020-05-05 06:40:11 +0200

    Page maternité

--- source/sites/mon-entreprise.fr/sitePaths.ts
+++ source/sites/mon-entreprise.fr/sitePaths.ts
@@ -57,7 +57,8 @@ const sitePathsFr = {
 		'auto-entrepreneur': '/auto-entrepreneur',
 		comparaison: '/comparaison-régimes-sociaux',
 		salarié: '/salarié',
-		'artiste-auteur': '/artiste-auteur'
+		'artiste-auteur': '/artiste-auteur',
+		maternité: '/maternité'
 	},
 	économieCollaborative: {
 		index: '/économie-collaborative',
@@ -101,6 +102,7 @@ const sitePathsEn = {
 		déclarationIndépendant: '/declaration-aid-independent'
 	},
 	simulateurs: {
+		...sitePathsFr.simulateurs,
 		index: '/simulators',
 		'assimilé-salarié': '/assimile-salarie',
 		indépendant: '/independant',
